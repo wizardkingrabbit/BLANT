7d02e05a84fa522e50a5dfc7997316d0a069a095
wizardkingrabbit <jingtil2@uci.edu>
2021-03-01 05:07:29 +0800

    py-C merge success

--- Dijkstra/builder.py
+++ Dijkstra/builder.py
@@ -17,7 +17,8 @@ build_graph() takes a file in the following format:
 Node 1 Node 2
 and returns a graph with every edge Node 1 <-> Node 2
 """
-
+from ctypes import *
+libCalc = CDLL("./libcalci.so")
 
 def convert_graph_int(file):
     nodes = {}
@@ -42,6 +43,7 @@ def convert_graph_int(file):
 
 def build_graph(file):
     count = 0
+    print ("new builder is running")
     g = Graph()
     for line in open(file):
         n1, n2 = line.split()
@@ -60,7 +62,10 @@ def build_graph(file):
     for line in open(file):
         n1, n2 = line.split()
         g.build_adjmat(g.indexes[n1], g.indexes[n2])
-
+    c_graph_pointer = libCalc.GraphAlloc(count, True, True)
+    for line in open(file):
+        n1, n2 = line.split()
+        c_graph_pointer = libCalc.GraphConnect(c_graph_pointer, g.indexes[n1], g.indexes[n2])
     return g
 
 """
